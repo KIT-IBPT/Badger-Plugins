b8a69fe36418fa39524e25267bb66c9006e32974
merrittzhang <zhangmerritt@gmail.com>
2023-08-16 13:58:43 +0200

    casting error

--- environments/Ackley/__init__.py
+++ environments/Ackley/__init__.py
@@ -20,7 +20,7 @@ def Ackley(individual):
     b = 0.2
     c = 2 * np.pi
 
-    objectives = a + np.exp
+    objectives = float(a + np.exp)
     
     for i in range(10):
         objectives += (-a * np.exp(-b * (1/10) * xi_values[i] ** 2))
